5809e6e88be01b6caf900074610742a9b115da2a
jerryr <jerry@163.com>
2021-07-31 15:55:53 +0800

    hot-fix commit1

--- src/pers/jerryr/array/GitTest.java
+++ src/pers/jerryr/array/GitTest.java
@@ -3,5 +3,6 @@ package pers.jerryr.array;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
